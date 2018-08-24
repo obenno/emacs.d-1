4beb93547b7b957c66e57bed5cd016259dde6fa4
obenno <obenno@users.noreply.github.com>
2018-08-24 14:33:25 +0800

    Changed color theme from molokai to klere

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -1,4 +1,8 @@
-(load-theme 'molokai t)
+;; Add custom theme path
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+
+(load-theme 'klere t)
 
 ;; This line must be after color theme setup! Don't know why.
 (setq color-theme-illegal-faces "^\\(w3-\\|dropdown-\\|info-\\|linum\\|yas-\\|font-lock\\|dired-directory\\)")
